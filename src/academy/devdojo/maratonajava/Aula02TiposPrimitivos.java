732017e207cea5b93799cf6ab3b4ad67c1c20ac5
limagl <carvalhoworkspace@gmail.com>
2026-01-05 18:20:06 -0300

    s

--- src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
@@ -3,5 +3,6 @@ package academy.devdojo.maratonajava;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         System.out.println("Ola");
+        System.out.println("sas");
     }
 }
