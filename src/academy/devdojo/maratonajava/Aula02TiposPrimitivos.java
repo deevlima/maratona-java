5a8b33cd4e1dc3d5363225167c16a85d52d5837c
limagl <carvalhoworkspace@gmail.com>
2026-01-08 16:23:27 -0300

    feat: exercício sobre tipos primitivos

--- src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
@@ -2,6 +2,9 @@ package academy.devdojo.maratonajava;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
-
+        int idade = 10;
+        double valor = 23.2;
+        boolean condicao = true;
+        char sigla = 'g';
     }
 }
